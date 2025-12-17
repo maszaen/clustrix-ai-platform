454d0b83ddcedf85d5e9ada0e1d6e2cfe191393a
maszaen <exqeon@gmail.com>
2025-12-18 06:58:21 +0700

    Use patch-package for streamdown-rn customizations (EAS build compatible)

--- mobile/local_modules/streamdown-rn/dist/components/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Component Exports
- *
- * Skeleton primitives for streamdown-rn.
- */
-// Skeleton primitives
-export { Skeleton, SkeletonText, SkeletonRect, SkeletonCircle, SkeletonNumber, } from './Skeleton';
-//# sourceMappingURL=index.js.map
--- mobile/local_modules/streamdown-rn/dist/core/splitter/blockClosers.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Helpers to detect when special blocks are closed.
- */
-export declare function isCodeBlockClosed(content: string): boolean;
-export declare function isComponentClosed(content: string): boolean;
-//# sourceMappingURL=blockClosers.d.ts.map
